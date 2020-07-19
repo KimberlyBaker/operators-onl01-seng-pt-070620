aa3250511566ab4b7a72103caa095b08cb1af5d2
Kimberly Baker <bakerk2010@gmail.com>
2020-07-19 20:37:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,26 @@
 def unsafe?(speed)
-
+  if speed < 40
+    true
+  elsif speed > 60
+    true
+  else
+    false
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+	if speed < 40 || speed > 60 ? true : false
+	end
 end
-	
 
+unsafe?(79)
 
+unsafe?(35)
+
+unsafe?(50)
+
+not_safe?(79)
+
+not_safe?(35)
+
+not_safe?(50)
